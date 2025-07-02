e5ef3af015389b3066fdd7e073f635f52c5ae006
Josh Bajorek <milk18001955@gmail.com>
2025-07-02 23:05:46 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <a class="hover:te

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,9 +28,6 @@ export default function LandingPage() {
             <Link href="#" className="hover:text-black dark:hover:text-white transition-colors">
               Sign up
             </Link>
-            <Link href="#" className="hover:text-black dark:hover:text-white transition-colors">
-              Features
-            </Link>
             <button onClick={() => setIsReviewModalOpen(true)} className="hover:text-black dark:hover:text-white transition-colors">
               Review
             </button>
@@ -93,9 +90,9 @@ export default function LandingPage() {
 
           <div className="py-24 md:py-32 space-y-8 md:space-y-12">
             <h2 className="text-6xl md:text-8xl lg:text-9xl font-extrabold tracking-tighter text-black dark:text-white">
-              One Login. One Matrix.
+                One Login. One Matrix.
             </h2>
-            <h3 className="text-4xl md:text-5xl lg:text-6xl font-extrabold tracking-tighter text-black dark:text-white mt-12 md:mt-16">
+            <h3 className="text-6xl md:text-8xl lg:text-9xl font-extrabold tracking-tighter text-black dark:text-white mt-8">
               Analytics that power your business.
             </h3>
           </div>
--- src/app/rmd-matrix/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-// This page has been neutralized to prevent build errors due to a
-// duplicate component declaration with /contribution-matrix.
-export default function RMDMatrixPage() {
-    return null;
-}
