24e2f924402c3a8e3e316283b675afe7423c4f1c
Josh Bajorek <milk18001955@gmail.com>
2025-07-02 22:49:23 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <h2 class="text-6x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -44,10 +44,6 @@ export default function LandingPage() {
             matrix
           </h1>
           
-          <h2 className="text-6xl md:text-8xl lg:text-9xl font-extrabold tracking-tighter text-black dark:text-white">
-            One Login. One Matrix.
-          </h2>
-
           <div className="flex items-center justify-center mt-8 gap-4">
             <p className="text-4xl md:text-5xl text-black dark:text-white font-bold tracking-wide">
                 see how deep the rabbit hole goes…
@@ -94,6 +90,9 @@ export default function LandingPage() {
           </div>
           
           <div className="py-24 md:py-32">
+            <h2 className="text-6xl md:text-8xl lg:text-9xl font-extrabold tracking-tighter text-black dark:text-white">
+              One Login. One Matrix.
+            </h2>
             <h3 className="text-6xl md:text-8xl lg:text-9xl font-extrabold tracking-tighter text-black dark:text-white mt-12 md:mt-16">
               Analytics that power your business.
             </h3>
