dd1cb41b5eed952dc0040b615f3404dcf5666634
Josh Bajorek <milk18001955@gmail.com>
2025-08-30 16:17:25 +0000

    The Code Solution 🎨 The goal is to change the color of that border to b

--- src/components/AppShell.tsx
+++ src/components/AppShell.tsx
@@ -34,11 +34,11 @@ export default function AppShell({ children }: { children: React.ReactNode }) {
         } as React.CSSProperties
       }
     >
-      <header className="fixed inset-x-0 top-0 z-40 h-[var(--hh)] border-b border-border bg-background/60 backdrop-blur">
+      <header className="fixed inset-x-0 top-0 z-40 h-[var(--hh)] border-b border-border/50 bg-background/60 backdrop-blur">
         <TopToolbar onToggleCollapsed={handleSidebarToggle} collapsed={collapsed} />
       </header>
       <aside
-        className="fixed left-0 top-[var(--hh)] z-30 h-[calc(100vh-var(--hh))] border-r border-border bg-background overflow-hidden transition-[width] duration-200"
+        className="fixed left-0 top-[var(--hh)] z-30 h-[calc(100vh-var(--hh))] border-r border-sidebar-border bg-background overflow-hidden transition-[width] duration-200"
         style={{ width: "var(--sbw)" }}
       >
         <Sidebar
