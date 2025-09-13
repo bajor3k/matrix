e7c51725b9dc646c806f86df6631d86770724f1c
Josh Bajorek <milk18001955@gmail.com>
2025-09-13 15:05:52 +0000

    make this font bigger (_for element h1_)

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,7 +2,7 @@
 export default function DashboardPage() {
   return (
     <main className="min-h-dvh p-6">
-      <h1 className="text-xl font-semibold">Welcome Josh</h1>
+      <h1 className="text-3xl font-semibold">Welcome Josh</h1>
     </main>
   );
 }
