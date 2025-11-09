f5634c8b94852dbb493f0b82759c23df95a36236
Josh Bajorek <milk18001955@gmail.com>
2025-11-09 16:49:36 +0000

    when a response is generated, i should be able to edit it (_for element

--- src/app/terminal/page.tsx
+++ src/app/terminal/page.tsx
@@ -218,7 +218,7 @@ Reminder: Please attach the following document:
                 placeholder="The generated response will appear here..."
                 className="h-full min-h-[320px] resize-none bg-input/50 pr-28"
                 value={response}
-                readOnly
+                onChange={(e) => setResponse(e.target.value)}
               />
                <a
                   href={response ? createMailtoLink() : undefined}
