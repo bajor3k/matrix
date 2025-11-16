757d2f19def31606c9fd79f4d28f9599551277a2
Josh Bajorek <milk18001955@gmail.com>
2025-11-16 18:40:40 +0000

    Delete this (_for element h1_)

--- src/app/margin/page.tsx
+++ src/app/margin/page.tsx
@@ -2,7 +2,7 @@
 export default function MarginPage() {
   return (
     <main className="min-h-screen flex-1 p-6 space-y-6 md:p-8">
-      <h1 className="text-3xl font-bold tracking-tight text-foreground">Margin</h1>
+      
     </main>
   );
 }
