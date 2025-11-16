95fcac5b7f5b966a22d37eb03befbeec08d5f4d0
Josh Bajorek <milk18001955@gmail.com>
2025-11-16 19:17:21 +0000

    Good catch. You’re right: that top area should be a full card + table fo

--- src/app/margin/page.tsx
+++ src/app/margin/page.tsx
@@ -1,11 +1,11 @@
 // src/app/margin/page.tsx
-import MarginActiveCallsCard from "./components/MarginActiveCallsCard";
+import MarginCallTable from "./components/MarginCallTable";
 import MarginNearCallTable from "./components/MarginNearCallTable";
 
 export default function MarginPage() {
   return (
     <main className="min-h-screen flex-1 p-6 space-y-6 md:p-8">
-      <MarginActiveCallsCard />
+      <MarginCallTable />
       <MarginNearCallTable />
     </main>
   );
