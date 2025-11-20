4824d3d123b5197821084dbcb674c05cbfbbae56
Josh Bajorek <milk18001955@gmail.com>
2025-11-20 17:59:51 +0000

    Delete this (_for element h1_)

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,7 +2,6 @@
 export default function ProjectPage() {
   return (
     <main className="min-h-screen flex-1 p-6 space-y-6 md:p-8">
-      <h1 className="text-3xl font-bold tracking-tight text-foreground">Project</h1>
       {/* Page content will go here */}
     </main>
   );
