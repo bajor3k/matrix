64f29507338e503a244e1433ad7874a2919e39b6
josh bajorek <Bajor3k@gmail.com>
2026-01-01 17:33:16 +0000

    delete this (_for element h1_)

--- src/app/wall-street/page.tsx
+++ src/app/wall-street/page.tsx
@@ -1,7 +1,7 @@
 export default function WallStreetPage() {
   return (
     <div className="flex h-full flex-col p-8 text-white">
-      <h1 className="text-2xl font-bold">Wall Street</h1>
+      
     </div>
   );
 }
