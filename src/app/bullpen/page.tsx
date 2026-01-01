c6b7a622be800a4612153e28ae09af2161c427b3
josh bajorek <Bajor3k@gmail.com>
2026-01-01 00:00:42 +0000

    delete this line (_for element h1_)

--- src/app/bullpen/page.tsx
+++ src/app/bullpen/page.tsx
@@ -2,7 +2,7 @@
 export default function BullpenPage() {
   return (
     <div className="text-foreground p-6 md:p-10">
-      <h1 className="text-3xl font-bold mb-4">The Bullpen</h1>
+      
     </div>
   );
 }
