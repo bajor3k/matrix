af6f4729c0ca81bbdc9e3c99803b8b7618036c85
josh bajorek <Bajor3k@gmail.com>
2026-01-01 00:10:09 +0000

    delete this (_for element h1_)

--- src/app/crm/advisors/page.tsx
+++ src/app/crm/advisors/page.tsx
@@ -1,7 +1,7 @@
 export default function AdvisorCrmPage() {
   return (
     <div className="text-foreground p-6 md:p-10">
-      <h1 className="text-3xl font-bold mb-4">Advisor CRM</h1>
+      
     </div>
   );
 }
